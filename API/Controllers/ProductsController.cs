589bc08a296a846867d9de98cd0159cd96ce498e
RoxanaCristinaMarin <113606968+RoxanaCristinaMarin@users.noreply.github.com>
2023-07-19 06:16:07 +0300

    chore: added error handling and refactored program class

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,10 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
 	private readonly IGenericRepository<Product> _productsRepo;
 	private readonly IGenericRepository<ProductBrand> _productBrandRepo;
---
+++ API/Erros/ApiException.cs
@@ -0,0 +1,12 @@
+namespace API.Erros;
+
+public class ApiException : ApiResponse
+{
+	public ApiException(int statusCode, string message = null, string details = null) 
+		: base(statusCode, message)
+	{
+		Details = details;
+	}
+
+	public string Details { get; set; }
+}
